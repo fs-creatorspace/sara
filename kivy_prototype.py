57d4933fc4b3995f751e2cc1cfdaf4bee0f5dcf5
OliverKubesch <ok.freizeit@outlook.com>
2023-11-12 17:39:57 +0100

    Update kivy_prototype.py

--- kivy_prototype.py
+++ kivy_prototype.py
@@ -2,31 +2,41 @@ import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.uix.image import Image
 from kivy.uix.label import Label
 
-class GIFChangerApp(App):
+from conversation import Conversation  # Import the Conversation class from your module
+
+class ChatBotApp(App):
     def build(self):
         self.layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        self.output_label = Label(text="ChatBot Output:")
+        self.layout.add_widget(self.output_label)
 
-        # Initial GIF and label
-        self.initial_gif = Image(source="./img/initial.gif")  
-        self.layout.add_widget(self.initial_gif)
+        self.conversation = Conversation(path="conversation_data", load=0)  # Create an instance of the Conversation class
 
-        self.label = Label(text="Initial Value")
-        self.layout.add_widget(self.label)
+        # Create a button to call the textToSpeech function
+        self.text_to_speech_button = Button(text="Text to Speech")
+        self.text_to_speech_button.bind(on_press=self.text_to_speech)
+        self.layout.add_widget(self.text_to_speech_button)
 
-        # Button to change GIF and value
-        self.button = Button(text="Change GIF and Value")
-        self.button.bind(on_press=self.on_button_click)
-        self.layout.add_widget(self.button)
+        # Create a button to call the speak function
+        self.speak_button = Button(text="Speak")
+        self.speak_button.bind(on_press=self.speak)
+        self.layout.add_widget(self.speak_button)
 
         return self.layout
 
-    def on_button_click(self, instance):
-        # Change the GIF and label text
-        self.initial_gif.source = "./img/changed.gif" 
-        self.label.text = "New Value"
+    def text_to_speech(self, instance):
+        # Example: Call the textToSpeech function from the Conversation class
+        question = "Hello, this is a test question."
+        audio_path = self.conversation.textToSpeech(question)
+        self.output_label.text = f"Text to Speech function called. Audio saved at {audio_path}"
+
+    def speak(self, instance):
+        # Example: Call the speak function from the Conversation class
+        audio_path = "path_to_audio.mp3"  # Replace with the actual path to an audio file
+        self.conversation.speak(audio_path)
+        self.output_label.text = "Speak function called."
 
 if __name__ == '__main__':
-    GIFChangerApp().run()
+    ChatBotApp().run()
